b22b47abb379769a3a45001c2c474ae58dc3db00
MUHAMMAD ATHAR SAJID <athersajid620@gmail.com>
2024-10-01 22:39:34 +0500

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+mongoose.connect(`mongodb://127.0.0.1:27017/crudApp`)
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    imageUrl: String
+})
+
+
+module.exports = mongoose.model("user", userSchema);
